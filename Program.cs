e3206022ee13c57e9702105031e4e6fa77d4f940
samsung <demonov03@gmail.com>
2024-04-07 23:55:01 +0300

    req

---
+++ Program.cs
@@ -0,0 +1,48 @@
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortName = phoneBook.OrderBy(name => name.Name);
+
+            Console.WriteLine("Сортировка контактов по имени:");
+
+            foreach (var name in sortName)
+                Console.WriteLine(name.Name);
+
+            Console.WriteLine();
+
+            var sortLastName = phoneBook.OrderBy(name => name.LastName);
+
+            Console.WriteLine("Сортировка контактов по фамилии:");
+
+            foreach (var name in sortLastName)
+                Console.WriteLine(name.LastName);
+        }
+    }
+    public class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
